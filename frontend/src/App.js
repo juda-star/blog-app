58da14dc905514ac6062e57fc12cc2f69fc1bb82
juda-star <juda.ishta@gmail.com>
2023-02-03 03:48:26 +0200

    forontend

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import Header from "./components/Header/Header";
+import React from "react";
+import Auth from "./components/Login/Auth";
+import Blogs from "./components/Blogs/Blogs";
+import UserBlogs from "./components/UserBlogs/UserBlogs";
+import BlogDetail from "./components/BlogDetail/BlogDetail";
+import AddBlog from "./components/AddBlog/AddBlog";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {useSelector} from "react-redux"
+function App() {
+  const isLoggedIn =useSelector((state)=>state.isLoggedIn)
+  return (
+    <>
+      <Router>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <Routes>
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/blog/add" element={<AddBlog />} />
+            <Route path="/myBlogs" element={<UserBlogs />} />
+            <Route path="/myBlogs/:id" element={<BlogDetail />} />
+          </Routes>
+        </main>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import {useSelector} from "react-redux"
+
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Box,
+  Button,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+const Header = () => {
+    const isLoggedIn =useSelector((state)=>state.isLoggedIn)
+
+    const [value,setValue]=useState()
+  return (
+    <AppBar
+      sx={{
+        position: "sticky",
+        background: "linear-gradient(45deg, blue, red)",
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h4">BlogsApp</Typography>
+        {isLoggedIn&&<Box display="flex" marginLeft={"auto"} marginRight="auto">
+          <Tabs textColor="inherit" value={value} onChange={(e,val)=>setValue(val)}>
+            <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
+            <Tab LinkComponent={Link} to="/myBlog" label="My Blogs" />
+          </Tabs>
+          </Box>}
+
+          <Box display="flex" marginLeft="auto">
+
+          <Button
+          LinkComponent={Link} to="/auth"
+            variant="conatained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            Login
+          </Button>
+          <Button
+          LinkComponent={Link} to="/auth"
+            variant="conatained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            SignUp
+          </Button>
+         {isLoggedIn && <Button
+         LinkComponent={Link} to="/auth"
+            variant="conatained"
+            sx={{ margin: 1, borderRadius: 10 }}
+            color="warning"
+          >
+            Logout
+          </Button>}
+          
+
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
